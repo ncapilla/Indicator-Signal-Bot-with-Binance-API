071eec34c3f3e0b2e726ef9d3b7ea630765b4681
Nacho Capilla <nachocapilla@gmail.com>
2021-11-02 12:29:48 +0000

    Add requirements file and setup script for TA-Lib

---
+++ install-ta-lib.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt install libpython3.9-dev
+sudo apt install libtool-bin
+
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+
+PREFIX=/usr
+./configure --prefix=$PREFIX
+make
+sudo make install
+
+# Do whatever it does
+libtool --finish /usr/lib
+
+rm -rf ta-lib-0.4.0-src.tar.gz
+rm -rf ta-lib/
+
+export TA_LIBRARY_PATH=${PREFIX}/lib
+export TA_INCLUDE_PATH=${PREFIX}/include
+
+pip install ta-lib
+
